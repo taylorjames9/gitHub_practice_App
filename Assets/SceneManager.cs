64ed97f0bf924972ab21395fffe7f6091b04eb40
taylorjames9 <taylorjames9@gmail.com>
2015-02-24 19:16:03 -0800

    pretty decent working version. after adding master scene animation manager. Also am adding question dynamically at runtime

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -1,51 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour {
 
+	public GameObject qPrefab;
 
 	private int workState;
 	//enum StagesOfProduction {Pre, First, Second, Third, Fourth, Working};
 
-	void Start(){
-		//StagesOfProduction myStagesOfProduction;
-		//myStagesOfProduction = StagesOfProduction.Pre;
+	//These are listed in the order that they will appear. 
+	public  List<GameObject> allLinesInGame = new List<GameObject>();
+	public  List<GameObject> allQuestionsInGame = new List<GameObject>();
+	public  List<GameObject> allDotsInGame = new List<GameObject>();
+
+	public  List<GameObject> allNarrationsInGame = new List<GameObject>();
+	public  List<GameObject> allWeirdAnimationsInGame = new List<GameObject>();
+	private int currLineSeg;
+	private int currQuestionsInGame;
+	private int currDotsInGame;
+
+	void OnEnable()
+	{
+		Debug.Log ("allLinesInGame = "+allLinesInGame.Count);
+		TriggerLineFinish.OnLineFinish += NextLineSeg;
+	}
+	
+	
+	void OnDisable()
+	{
+		TriggerLineFinish.OnLineFinish -= NextLineSeg;
+	}
+
+	void NextLineSeg()
+	{
+
+		//switch to new line Segment
+		currLineSeg++;
+		Debug.Log ("currLineSeg = "+currLineSeg);
+		if(currLineSeg < allLinesInGame.Count){
+			allLinesInGame[currLineSeg].SetActive(true);
+		}
+	}
+
+	void NextQuestion(){
+		Debug.Log (" question box should appear ");
+		GameObject myQWhole = GameObject.Instantiate(qPrefab) as GameObject;
+		//myQWhole.transform.parent = GameObject.Find("Canvas").transform;
+		myQWhole.transform.SetParent(GameObject.Find("Canvas").transform);
+		myQWhole.GetComponent<RectTransform>().localPosition = new Vector3(-100,-100,0);
+		Debug.Log ("myLocalPosition = "+myQWhole.GetComponent<RectTransform>().localPosition);
 
 	}
 
-	void Update(){
-		switch(workState){
+	void NextDot(){
 
-
-			//Case 0 will be the working state. Ie played when the line is drawing
-			case 0:
-			//draw line
-			//play Narration
-			//Maybe show bried Animation
-
-
-
-			break;
-
-			case 1:
-			//Display myQuestionWindow
-			//SetMyQuestion.text = " ";
-			//Evaluate answer
-			//go back to working state (above)
-
-
-			break;
-
-			case 2:
-				
-			break;
-
-
-
+		//switch on new Question
+		currDotsInGame++;
+		Debug.Log ("currDot = "+currDotsInGame);
+		if(currDotsInGame < allDotsInGame.Count){
+			allDotsInGame[currDotsInGame].SetActive(true);
 		}
 
-
 	}
-
-
 }
---
+++ Assets/TriggerLineFinish.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerLineFinish : MonoBehaviour {
+
+	public delegate void LineFinishAction();
+	public static event LineFinishAction OnLineFinish;
+
+	
+	public IEnumerator lineFinish(float delay){
+		yield return new WaitForSeconds(delay);
+		OnLineFinish();
+
+	}
+}
